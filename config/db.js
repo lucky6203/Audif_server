b8de9ad72816074606dd0dfe60db5c51df418009
lucky kumar <lk502295@gmail.com>
2025-07-17 23:09:02 +0530

    fix cloud stroge

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI →", process.env.MONGO_URI); // Debug
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
