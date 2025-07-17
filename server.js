09abbf89d3c24ff089ac70819979a73577bb1948
lucky kumar <lk502295@gmail.com>
2025-07-17 22:47:38 +0530

    fix db bug

--- server.js
+++ server.js
@@ -18,17 +18,10 @@ app.use(express.json());
 // Routes
 app.use('/api', authRoutes);
 
-// MongoDB connection
-// mongoose
-//   .connect(process.env.MONGO_URI , {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log('MongoDB connected');
-//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//   })
-//   .catch((err) => console.error('MongoDB connection error:', err));
+app.use(cors({
+  origin: "http://localhost:5173", // Your Vite frontend origin
+  credentials: true // if you're using cookies or auth headers
+}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
